2a3cc4d3642d1afba773cb978e58a6b752edddf0
zacksinclair <zacksinclair@gmail.com>
2022-04-24 10:34:04 -0700

    sometimes messages undefined, protect against it

--- src/client/actions/MessageCreate.js
+++ src/client/actions/MessageCreate.js
@@ -10,6 +10,10 @@ class MessageCreateAction extends Action {
     const client = this.client;
     const channel = this.getChannel(data);
     if (channel) {
+      if (!channel.messages) {
+        return {};
+      }
+      
       const existing = channel.messages.cache.get(data.id);
       if (existing) return { message: existing };
       const message = channel.messages._add(data);
